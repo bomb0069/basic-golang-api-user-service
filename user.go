c2da25abeae8b9f05a73e85a62f407adb31c3f8e
karan sivarat <bomb0069@gmail.com>
2021-01-17 15:27:51 +0700

    [Added]: getAllUsersHandler to handle api getAllUser to prepare for inject some of resource

--- user.go
+++ user.go
@@ -7,7 +7,13 @@ import (
 )
 
 func NewUserAPI(group *echo.Group) {
-	group.GET("/users", getUser)
+	group.GET("/users", getAllUsersHandler())
+}
+
+func getAllUsersHandler() func(echo.Context) error {
+	return func(ctx echo.Context) error {
+		return ctx.JSON(http.StatusOK, "Hello world")
+	}
 }
 
 func getUser(ctx echo.Context) error {
